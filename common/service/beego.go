3db390d5fd9df0333972b0ba34e52e2f565faeed
piupuer <396679132@qq.com>
2019-04-16 11:54:25 +0800

    新增实体指定列关联属性查询

--- common/service/beego.go
+++ common/service/beego.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"fmt"
 	"github.com/astaxie/beego/logs"
 	"github.com/astaxie/beego/orm"
 	"reflect"
@@ -8,18 +9,30 @@ import (
 )
 
 // 读取实体对象所有的关联实体
+// 谨慎使用该方法，该方法会造成性能影响。推荐使用 LoadModelRelated 方法，指定具体的关联属性查询
 func LoadAllRelated(model interface{}) {
-	// 读数据使用从库
-	o := LockOrmer(true)
-	defer UnlockOrmer(true)
 	pType := reflect.TypeOf(model).Elem()
+	var cols []string
 	for k := 0; k < pType.NumField(); k++ {
 		ormTag := pType.Field(k).Tag.Get("orm")
 		if strings.Contains(ormTag, "rel") || strings.Contains(ormTag, "reverse") {
-			_, err := o.LoadRelated(model, pType.Field(k).Name)
-			if err != nil && err != orm.ErrNoRows {
-				logs.Error("Load Related Error", err)
-			}
+			col := pType.Field(k).Name
+			cols = append(cols, col)
+		}
+	}
+	// 获取cols关联实体
+	LoadModelRelated(model, cols...)
+}
+
+// 获取指定列关联实体
+func LoadModelRelated(model interface{}, cols ...string) {
+	// 读数据使用从库
+	o := LockOrmer(true)
+	defer UnlockOrmer(true)
+	for _, col := range cols {
+		_, err := o.LoadRelated(model, col)
+		if err != nil && err != orm.ErrNoRows {
+			logs.Error(fmt.Sprintf("Load Related %s Error %s", col, err))
 		}
 	}
 }
