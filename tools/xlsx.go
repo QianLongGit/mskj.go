cdc702d7844ed1638227665e8822e737462f31d8
piupuer <396679132@qq.com>
2019-04-23 15:11:57 +0800

    excel可以对多页sheet进行重写

--- tools/xlsx.go
+++ tools/xlsx.go
@@ -38,40 +38,41 @@ func Read(excelFileName string, startRow int) ([]interface{}, error) {
 	return sheets, nil
 }
 
-// 重写sheet
+// 重写sheet, 一到多页
 // excelSrc源文件路径
 // excelDest目标文件路径
-// sheetData[0] {string} sheet名称
-// sheetData[1] {[][]string} 需要添加的数据
-func ReWriteSheet(excelSrc string, excelDest string, sheetData [2]interface{}) error {
+func ReWriteSheet(excelSrc string, excelDest string, sheetDatas ...[2]interface{}) error {
 	xlFile, err := xlsx.OpenFile(excelSrc)
 	if err != nil {
 		logs.Error(fmt.Sprintf("读取文档失败, 文件路径: %s", excelSrc))
 		return fmt.Errorf("读取文档失败, 文件路径: %s", excelSrc)
 	}
-	l := len(sheetData)
-	if l != 2 {
-		return fmt.Errorf("数据长度不符合要求, 重写sheet失败")
-	}
-	sheetName, ok := sheetData[0].(string)
-	if !ok {
-		return fmt.Errorf("sheet名称类型必须为string")
-	}
-	data, ok := sheetData[1].([][]string)
-	if !ok {
-		return fmt.Errorf("sheet数据类型必须为[][]string")
-	}
-	for _, sheet := range xlFile.Sheets {
-		if sheet.Name == sheetName {
-			// 添加数据到sheet
-			for _, rowData := range data {
-				row := sheet.AddRow()
-				for _, cellData := range rowData {
-					cell := row.AddCell()
-					cell.Value = cellData
+	for _, sheetData := range sheetDatas {
+		l := len(sheetData)
+		if l != 2 {
+			return fmt.Errorf("数据长度不符合要求, 重写sheet失败")
+		}
+		sheetName, ok := sheetData[0].(string)
+		if !ok {
+			return fmt.Errorf("sheet名称类型必须为string")
+		}
+		data, ok := sheetData[1].([][]string)
+		if !ok {
+			return fmt.Errorf("sheet数据类型必须为[][]string")
+		}
+		for _, sheet := range xlFile.Sheets {
+			// 找到名称相同的页
+			if sheet.Name == sheetName {
+				// 添加数据到sheet
+				for _, rowData := range data {
+					row := sheet.AddRow()
+					for _, cellData := range rowData {
+						cell := row.AddCell()
+						cell.Value = cellData
+					}
 				}
+				break
 			}
-			break
 		}
 	}
 	// 创建目标目录
