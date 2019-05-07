32d4f3f5b4f1579a30ecbd8e9f9618df422fac6b
piupuer <396679132@qq.com>
2019-05-07 12:46:12 +0800

    修改名称

--- tools/conversion.go
+++ tools/conversion.go
@@ -8,8 +8,8 @@ import (
 
 // 类型转换
 
-// map类型转为struct结构体, 中间通过json桥接
-func Map2StructByJson(m interface{}, s interface{}) error {
+// struct转为包含相同字段的struct结构体, 中间通过json桥接
+func Struct2StructByJson(m interface{}, s interface{}) error {
 	machineJson, err := json.Marshal(m)
 	if err != nil {
 		logs.Error(fmt.Sprintf("map转换json失败 异常 %s", err))
